90238ffd8add3be9572f495d423dedcbfe93c09c
SSH9753 <80192612+SSH9753@users.noreply.github.com>
2023-05-22 19:14:06 +0900

    Create 2743_StringLength.java

---
+++ SSH/20230522/2743_StringLength.java
@@ -0,0 +1,9 @@
+package other;
+import java.util.Scanner;
+public class _2743_StringLength {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String str = scan.nextLine();
+        System.out.println(str.length());
+    }
+}
