a85b312a82992a5988df904edee8108aaa17070e
SSH9753 <80192612+SSH9753@users.noreply.github.com>
2023-05-30 20:46:20 +0900

    Update and rename 11005_BaseConversion.java to 11005_BaseConversion2.java

--- SSH/20230530/11005_BaseConversion2.java
+++ SSH/20230530/11005_BaseConversion2.java
@@ -2,7 +2,7 @@ package other;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-public class _11005_BaseConversion {
+public class _11005_BaseConversion2 {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         long n,b;
