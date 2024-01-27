c18f7e552085415002289acfeeb52c1fcfcb87e7
voka <chdlswhd7@naver.com>
2024-01-27 22:30:11 +0900

    gogogo

--- OJS/2024/20240126/주사위 고르기/Main.java
+++ OJS/2024/20240126/주사위 고르기/Main.java
@@ -2,10 +2,10 @@ import java.util.*;
 
 class Pair{
     int win;
-    int same;
-    public Pair(int win, int same){
+    int draw;
+    public Pair(int win, int draw){
         this.win = win;
-        this.same = same;
+        this.draw = draw;
     }
 }
 
@@ -23,26 +23,22 @@ class Solution {
             choiceN[i] = i;
         }
         boolean[] visited = new boolean[total];
-        // 주사위를 고를 수 있는 경우의 수
         combination(choiceN, visited, 0, total, half);
-        int myN = choiceList.size() / 2; // 고를 수 있는 경우의 수가 N개 일때 1번을 A가 고르면 자동으로 B가 N번을 고르게 된다.
-        int curMaxWin = 0; // 최대 승리수를 구하기 위한 변수
+        int myN = choiceList.size() / 2;
+        int curMaxWin = 0;
         for(int i=0;i<myN;++i){
-            // A가 i번 초이스로 주사위를 굴렸을때 나올 수 있는 모든 경우의 수
             int[] aResult = getAllPossibleScore(choiceList.get(i), dice);
-            // B가 N번 초이스로 주사위를 굴렸을때 나올 수 있는 모든 경우의 수
             int[] bResult = getAllPossibleScore(choiceList.get(choiceList.size() -1 - i), dice);
-            // 전체 경기 수
             int totalGame = aResult.length * bResult.length;
-            // A의 승리, 비긴 수
+            
+            Arrays.sort(bResult);
             Pair aInfo = countAWin(aResult, bResult);
             int aWin = aInfo.win; 
-            int bWin = totalGame - aWin - aInfo.same;
+            int bWin = totalGame - aWin - aInfo.draw;
             //System.out.printf("%d, %d\n",aWin, bWin);
-            if(aWin < bWin){ //b의 승리 횟수가 더 많다면 
+            if(aWin < bWin){
                 if(curMaxWin < bWin){
                     curMaxWin = bWin;
-                    // 정담을 변경해 준다.
                     tempAnswer = choiceList.get(choiceList.size() -1 - i);
                 }
             }else{
@@ -52,14 +48,12 @@ class Solution {
                 }
             }
         }
-        // 정답 옮기기
         for(int i=0;i<tempAnswer.size();++i){
             answer[i] = tempAnswer.get(i) + 1;
             //System.out.printf("%d ", tempAnswer.get(i));
         }
         return answer;
     }
-    // combination을 통해 주사위를 고를 수 있는 경우의 수를 구한다. 
     public void combination(int[] arr, boolean[] visited, int s, int n, int r){
         if(r == 0){
             List<Integer> temp = new ArrayList<>();
@@ -75,7 +69,6 @@ class Solution {
             visited[i] = false;
         }
     }
-    // 가능한 스코어들을 모두 구해준다.
     public int[] getAllPossibleScore(List<Integer> choice, int[][] dice){
         int[] result = dice[choice.get(0)];
         for(int i=1;i<choice.size();++i){
@@ -83,7 +76,6 @@ class Solution {
         }
         return result;
     }
-    // 2개의 배열을 합쳐주는 과정이다. {1,2,3}, (1,2,3) => {1 + 1, 1 + 2, 1 + 3, ... 3 + 1, 3 + 2, 3 + 3}
     public int[] multiple2Array(int[] a, int[] b){
         int[] result = new int[a.length * b.length];
         for(int i=0;i<a.length;++i){
@@ -93,17 +85,15 @@ class Solution {
         }
         return result;
     }
-    // a의 승리 수, 비긴 수를 lowerbound와 upperbound로 계산한다.
     public Pair countAWin(int[] a, int[] b){
-        int aWin = 0, aSame = 0;
-        Arrays.sort(b);
+        int aWin = 0, draw = 0;
         for(int i=0;i<a.length;++i){
             int lowerBoundIndex = lowerBound(b, a[i]);
             int upperBoundIndex = upperBound(b, a[i]);
             aWin += lowerBoundIndex;
-            aSame += upperBoundIndex - lowerBoundIndex;
+            draw += upperBoundIndex - lowerBoundIndex;
         }
-        return new Pair(aWin, aSame);
+        return new Pair(aWin, draw);
     }
     
     public int lowerBound(int[] b, int target){
--- OJS/2024/20240127/n+1 카드게임/Main.java
+++ OJS/2024/20240127/n+1 카드게임/Main.java
@@ -1,59 +1,56 @@
 import java.util.*;
 class Solution {
-    static int start, n, myCoin, canRound = 0;
-    static Set<Integer> mySet = new HashSet<>();
+    static int start, n, myCoin, round = 0;
+    static Set<Integer> firstSet = new HashSet<>(), keepSet = new HashSet<>();
     public int solution(int coin, int[] cards) {
-        int answer = 0;
         myCoin = coin;
         n = cards.length;
         start = n/3;
         for(int i=0;i<start;++i){
-            mySet.add(cards[i]);
+            firstSet.add(cards[i]);
         }
-        howManyRoundICanProcess(); // 앞으로 버틸 수 있는 라운드 수
+        howManyRoundICanProcessUsingFirstSet(); // 앞으로 버틸 수 있는 라운드 수
         process(cards); // 최대로 버틸 수 있는 라운드 수 구하기
-        return canRound + 1;
+        return round + 1;
     }
-    public void howManyRoundICanProcess(){
-        canRound = 0;
+    public void howManyRoundICanProcessUsingFirstSet(){
+        round = 0;
         List<Integer> removeList = new ArrayList<>();
         boolean[] visited = new boolean[n+1];
-        for(int k : mySet){
+        for(int k : firstSet){
             if(visited[k]) continue;
             int target = n + 1 - k;
-            if(mySet.contains(target)){
+            if(firstSet.contains(target)){
                 visited[target] = true;
-                canRound++;
+                round++;
             }
         }
         for(int k : removeList){
-            mySet.remove(k);
+            firstSet.remove(k);
         }
-        //System.out.printf("canRound ==> %d\n", canRound);
+        //System.out.printf("round ==> %d\n", round);
     }
-    public boolean useMyCard(Set<Integer> temp){
+    public boolean useKeepCard(){
         //System.out.println("USE MY CARDS ONLY");
         int canUse = -1;
-        for(int one : temp){
+        for(int one : keepSet){
             int oneTarget = n+1 - one;
-            if(temp.contains(oneTarget)){
+            if(keepSet.contains(oneTarget)){
                 canUse = one;
                 break;
             }
         }
         if(canUse != -1){
-            temp.remove(canUse);
+            keepSet.remove(canUse);
             int target = n+1 - canUse;
             //System.out.printf("%d, %d\n", canUse, target);
-            temp.remove(target);
+            keepSet.remove(target);
             return true;
         }
         return false;
     }
     public void process(int[] cards){
-    
-        Set<Integer> tempSet = new HashSet<>();
-        for(int i=0;i<canRound + 1; ++i){
+        for(int i=0;i<round + 1; ++i){
             if(start + i*2 + 1 >= n) break; // 카드 사이즈가 넘어간다면
             if(myCoin == 0) break;// 코인을 다 써도 이미 라운드 수는 증가했기 때문에 종료시킨다.
             int one = cards[i*2 + start];
@@ -62,31 +59,31 @@ class Solution {
             boolean twoUse = false;
             int oneTarget = n+1 - one;
             int twoTarget = n+1 - two;
-            if(mySet.contains(oneTarget) && myCoin >= 1){ // 처음 받았던 카드 한개와 1번 카드 사용 (코인 - 1)
+            if(firstSet.contains(oneTarget) && myCoin >= 1){ // 처음 받았던 카드 한개와 1번 카드 사용 (코인 - 1)
                 //System.out.println("USE ONE COIN");
                 //System.out.printf("%d, %d\n", one, oneTarget);
                 myCoin -= 1;
-                mySet.remove(oneTarget);
+                firstSet.remove(oneTarget);
                 oneUse = true;
-                canRound += 1;
+                round += 1;
             }
-            if(mySet.contains(twoTarget) && myCoin >= 1){ // 처음 받았던 카드 한개와 2번 카드 사용 (코인 - 1)
+            if(firstSet.contains(twoTarget) && myCoin >= 1){ // 처음 받았던 카드 한개와 2번 카드 사용 (코인 - 1)
                 myCoin -= 1;
                 //System.out.println("USE ONE COIN");
                 //System.out.printf("%d, %d\n", two, twoTarget);
-                mySet.remove(twoTarget);
+                firstSet.remove(twoTarget);
                 twoUse = true;
-                canRound += 1;
+                round += 1;
             }
             // 안쓴 카드 저장
-            if(!oneUse) tempSet.add(one);
-            if(!twoUse) tempSet.add(two); 
+            if(!oneUse) keepSet.add(one);
+            if(!twoUse) keepSet.add(two); 
             
-            if(canRound <= i && myCoin >= 2){// 이전에 나왔던 카드 중 쓰지 않은 카드 2개 사용 (코인 - 2)
+            if(round <= i && myCoin >= 2){// 이전에 나왔던 카드 중 쓰지 않은 카드 2개 사용 (코인 - 2)
                 //System.out.println("USE TWO COIN IN TEMP");
-                if(useMyCard(tempSet)){
+                if(useKeepCard()){
                     myCoin -= 2;
-                    canRound += 1;
+                    round += 1;
                 }
                 
             }
