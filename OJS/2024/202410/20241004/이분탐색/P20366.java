398719ea938b096c260cf7b3b77b4f2f1f917cb9
voka <chdlswhd7@naver.com>
2024-10-04 23:21:51 +0900

    주석 추가

--- OJS/2024/202410/20241004/이분탐색/P20366.java
+++ OJS/2024/202410/20241004/이분탐색/P20366.java
@@ -21,18 +21,19 @@ public class P20366 {
         }
         Arrays.sort(myList);
         int minGap = Integer.MAX_VALUE;
-        for(int i=0;i<N-3;++i){
+        for(int i=0;i<N-3;++i){ // N-3 한 이유는 눈덩이가 총 4개 필요하기 때문에 
             for(int j=i+3;j<N;++j){
+                // 양쪽 값을 i, j 로 잡고 눈사람 만들기
                 int s = i+1;
                 int e = j-1;
                 while(s < e){
-                    int a = myList[i] + myList[j]; 
-                    int b = myList[s] + myList[e];
+                    int a = myList[i] + myList[j]; // 바깥쪽 눈덩이
+                    int b = myList[s] + myList[e]; // 이분탐색할 안쪽 눈덩이
                     minGap = Math.min(minGap,Math.abs(a-b));
-                    if(a > b){
-                        s++;
-                    }else{
-                        e--;
+                    if(a > b){ // 바깥쪽 눈덩이가 크면
+                        s++; // 안쪽 눈덩이의 덩치를 키워서 차이가 적게 만든다
+                    }else{ // 바깥쪽 눈덩이가 작으면
+                        e--; // 안쪽 눈덩이의 덩치를 줄여서 차이가 적게 나게 만든다
                     }
                 }
             }
