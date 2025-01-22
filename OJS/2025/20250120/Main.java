5a5bee3d7ebebf9e2f4e4f0ebfd8bd63a7b96cd2
voka <chdlswhd7@naver.com>
2025-01-22 23:13:42 +0900

    미지의 공간 탈출

--- OJS/2025/20250120/Main.java
+++ OJS/2025/20250120/Main.java
@@ -20,14 +20,17 @@ class My implements Comparable<My>{
         if(this.t < o.t) return -1;
         return 1;
     }
+    public void print(){
+        System.out.printf("(%d, %d) ", x, y);
+    }
 
 }
 public class Main {
-    static int WALL = 1,START = 2, TIMEZONE = 3,OUT = 4, ELE = 5, TIMESTOP = 7; // 상수
-    static int N,M,F;
+    static int WALL = 1,START = 2, TIMEZONE = 3,OUT = 4, ELE = 10, TIMESTOP = 7, PASS = 8; // 상수
+    static int N,M,F,LAST;
     static int tsx,tsy,sx,sy,tx,ty; // 타임머신 위치, 탈출구 위치
     static int[] dy = {1,-1,0,0}, dx = {0,0,1,-1}; // 2차원 평면 길찾기
-    static int[][] map;
+    static int[][] map, nextX, nextY;
     static int[][] tdmap, tempMap;
     /* 
     정육면체 전개도 생각해서 
@@ -49,7 +52,7 @@ public class Main {
         F = Integer.parseInt(st.nextToken());
         tempMap = new int[N][N];
         tdmap = new int[3*M][3*M];
-        map = new int[N + 2*M][N + 2*M];
+        map = new int[N + 2*M][N + 2*M]; nextX = new int[N + 2*M][N + 2*M]; nextY = new int[N + 2*M][N + 2*M];
 
         for(int i=0;i<3*M;++i){
             for(int j=0;j<3*M;++j){
@@ -63,9 +66,7 @@ public class Main {
             for(int j=0;j<N;++j){
                 int temp = Integer.parseInt(st.nextToken());
                 tempMap[i][j] = temp;
-                if(temp == OUT){ //탈출구
-                    tx = i; ty = j;
-                }else if(!timeflag && temp == TIMEZONE){
+                if(!timeflag && temp == TIMEZONE){
                     tsx = i; tsy = j;
                     timeflag = true;
                 }
@@ -78,16 +79,41 @@ public class Main {
                     int temp = Integer.parseInt(st.nextToken());
                     if(k == 0){ // 동쪽
                         tdmap[M + i][2*M + j] = temp;
+                        if(i == 0){
+                            nextX[M+i+tsx][2*M+j+tsy] = M+i+tsx - j - 1;
+                            nextY[M+i+tsx][2*M+j+tsy] = 2*M + tsy - 1;
+                            //반대편
+                            nextX[M+i+tsx - j - 1][2*M + tsy - 1] = M+i+tsx;
+                            nextY[M+i+tsx - j - 1][2*M + tsy - 1] = 2*M+j+tsy;
+                        }
+                        if(i == M-1){
+                            nextX[M+i+tsx][2*M+j+tsy] = M+i+tsx + j + 1;
+                            nextY[M+i+tsx][2*M+j+tsy] = 2*M + tsy - 1;
+                            //반대편
+                            nextX[M+i+tsx + j + 1][2*M + tsy - 1] = M+i+tsx;
+                            nextY[M+i+tsx + j + 1][2*M + tsy - 1] = 2*M+j+tsy;
+                        }
                     }else if(k == 1){ // 서쪽
                         tdmap[M + i][j] = temp;
+                        if(i == 0){
+                            nextX[M+i+tsx][j+tsy] = i+tsx + j;
+                            nextY[M+i+tsx][j+tsy] = tsy + M;
+                            //반대편
+                            nextX[i+tsx + j][tsy + M] = M+i+tsx;
+                            nextY[i+tsx + j][tsy + M] = j+tsy;
+                        }
+                        if(i == M-1){
+                            nextX[M+i+tsx][j+tsy] = 2*M+i+tsx - j;
+                            nextY[M+i+tsx][j+tsy] = tsy + M ;
+                            //반대편
+                            nextX[2*M+i+tsx - j][tsy + M] = M+i+tsx;
+                            nextY[2*M+i+tsx - j][tsy + M] = j+tsy;
+                        }
                     }else if(k == 2){ // 남쪽
                         tdmap[2*M + i][M + j] = temp;
                     }else if(k == 3){ // 북쪽
                         tdmap[i][M + j] = temp;
                     }else{
-                        if(temp == 2){// 타임머신 위치 -> 출발지
-                            sx = i; sy = j;
-                        }
                         tdmap[M + i][M + j] = temp;
                     }
                 }
@@ -100,19 +126,21 @@ public class Main {
             st = new StringTokenizer(br.readLine());
             r[i] = Integer.parseInt(st.nextToken());
             c[i] = Integer.parseInt(st.nextToken());
+            tempMap[r[i]][c[i]] = ELE + i;
             d[i] = Integer.parseInt(st.nextToken());
             v[i] = Integer.parseInt(st.nextToken());
         }
 
         turnL(M, 2*M, 2*M, 3*M);
         turnR(M, 0, 2*M, M);
-        turn180(0,M,M,2*M);
+        turnL(0,M,M,2*M);
+        turnL(0,M,M,2*M);
         for(int i=0;i<N + 2*M;++i){
             for(int j=0;j<N + 2*M;++j){
                 if((tsx - 1 <= i) && (i <= tsx + 3*M)&& (tsy -1 <= j) && (j <= tsy + 3*M)){
                     map[i][j] = WALL;
                 }
-                else map[i][j] = TIMESTOP;
+                else map[i][j] = PASS;
             }
         }
         //3dmap => map
@@ -131,42 +159,72 @@ public class Main {
         for(int i=tsx + 3*M;i<N + 2*M;++i){
             for(int j=0;j<tsy;++j){
                 map[i][j] = tempMap[i - 2*M][j];
+                changeELE(i - 2*M,j,i,j);
             }   
         }
         for(int i=0;i<tsx;++i){
             for(int j=tsy + 3*M;j<N + 2*M;++j){
                 map[i][j] = tempMap[i][j-2*M];
+                changeELE(i,j-2*M,i,j);
             }   
         }
         for(int i=tsx + 3*M;i<N + 2*M;++i){
             for(int j=tsy + 3*M;j<N + 2*M;++j){
                 map[i][j] = tempMap[i-2*M][j-2*M];
+                changeELE(i-2*M,j-2*M,i,j);
             }   
         }
         //상,하,좌,우
         for(int i=tsx + M;i<tsx + 2*M;++i){
             for(int j=0;j<tsy;++j){
                 map[i][j] = tempMap[i-M][j];
+                changeELE(i-M,j,i,j);
             }
         }
         for(int i=0;i<tsx;++i){
             for(int j=tsy+M;j<tsy+2*M;++j){
                 map[i][j] = tempMap[i][j-M];
+                changeELE(i,j-M,i,j);
             }
         }
         for(int i=tsx + M;i<tsx + 2*M;++i){
             for(int j=tsy+3*M;j<N + 2*M;++j){
                 map[i][j] = tempMap[i-M][j-2*M];
+                changeELE(i-M,j-2*M,i,j);
             }
         }
         for(int i=tsx+3*M;i<N + 2*M;++i){
             for(int j=tsy+M;j<tsy+2*M;++j){
                 map[i][j] = tempMap[i-2*M][j-M];
+                changeELE(i-2*M,j-M,i,j);
             }
         }
         
-        printMap();
-        
+        boolean findS = false, findO = false;
+        for(int i=0;i<N+2*M;++i){
+            for(int j=0;j<N+2*M;++j){
+                if(map[i][j] == OUT){
+                    tx = i; ty = j;
+                    findO = true;
+                }else if(map[i][j] == START){
+                    sx = i; sy = j;
+                    findS = true;
+                }
+                if(findO && findS) break;
+            }
+        }
+        //printMap();
+        // for(int i=0;i<N + 2*M;++i){
+        //     for(int j=0;j<N + 2*M;++j){
+        //         System.out.printf("(%d,%d), ", nextX[i][j], nextY[i][j]);
+        //     }
+        //     System.out.println();
+        // }
+        solve();
+        // spread(14);
+        // spread(28);
+        // spread(42);
+        //printMap();
     }
     static void turnL(int sx, int sy, int ex, int ey){
         int R = ex - sx;
@@ -196,18 +254,12 @@ public class Main {
             }
         }
     }
-    static void turn180(int sx, int sy, int ex, int ey){
-        int R = ex - sx;
-        int[][] temp = new int[R][R];
-        for(int i=0;i<R;++i){
-            for(int j=0;j<R;++j){
-                temp[R-i-1][j] = tdmap[sx+i][sy+j];
-            }
-        }
-        for(int i=0;i<R;++i){
-            for(int j=0;j<R;++j){
-                tdmap[sx+i][sy+j] = temp[i][j];
-            }
+    static void changeELE(int x, int y, int nx , int ny){
+        if(tempMap[x][y] >= ELE){
+            int id = tempMap[x][y] - ELE;
+            map[nx][ny] = ELE;
+            r[id] = nx;
+            c[id] = ny;
         }
     }
     static void print3d(){
@@ -228,39 +280,90 @@ public class Main {
     }
 
     static void solve(){
-        int curT = 0;
-        boolean[][] visit = new boolean[N+3*M][N+3*M];
-        spread(curT); // 맨 처음 이상현상 만들기
-        PriorityQueue<My> pq = new PriorityQueue<>();
-        pq.add(new My(sx,sy,0));
-        while(!pq.isEmpty()){
-            My temp = pq.poll();
-            System.out.println(temp.t);
-            if(curT != temp.t){
-                curT = temp.t;
-                spread(curT);
+        int _MAX = 500 * 500;
+        boolean[][] visit = new boolean[N+2*M][N+2*M];
+        int[][] cost = new int[N+2*M][N+2*M];
+        LAST = 0;
+        for(int i=0;i<N+2*M;++i){
+            for(int j=0;j<N+2*M;++j){
+                cost[i][j] = _MAX;
             }
         }
-
+        PriorityQueue<My> pq = new PriorityQueue<>();
+        pq.add(new My(sx,sy,0));
+        visit[sx][sy] = true;
+        cost[sx][sy] = 0;
+        while(!pq.isEmpty()){
+            My temp = pq.poll();
+            for(int i=0;i<4;++i){
+                int nx = temp.x + dx[i];
+                int ny = temp.y + dy[i];
+                if(!check2d(nx, ny)) continue;
+                if(map[nx][ny] == TIMESTOP){
+                    int tnx = nextX[temp.x][temp.y];
+                    int tny = nextY[temp.x][temp.y];
+                    if(!check2d(tnx, tny)) continue;
+                    nx = tnx; ny = tny;
+                }
+                if(map[nx][ny] == PASS){
+                    while(map[nx][ny] == PASS){
+                        nx = nx + dx[i];
+                        ny = ny + dy[i];
+                    }
+                    if(!check2d(nx, ny)) continue;
+                }
+                if(cost[nx][ny] > temp.t + 1){
+                    if(LAST < temp.t + 1){
+                        LAST = temp.t + 1;
+                        spread(temp.t+1);
+                    }
+                    cost[nx][ny] = temp.t + 1;
+                    pq.add(new My(nx, ny, temp.t + 1));
+                }
+                
+            }
+        }
+        // for(int i=0;i<N+2*M;++i){
+        //     for(int j=0;j<N+2*M;++j){
+        //         if(cost[i][j] == _MAX){
+        //             System.out.print("00 ");
+        //         }
+        //         else System.out.printf("%d ", cost[i][j]);
+        //     }
+        //     System.out.println();
+        // }
+        if(cost[tx][ty]==_MAX) System.out.println(-1);
+        else System.out.println(cost[tx][ty]);
 
     }
     static boolean check2d(int x, int y){
-        return x >= 0 && y >= 0 && x < N && y < N && (map[x][y] != 1) && (map[x][y] != 4) && (map[x][y] != ELE);   
+        return x >= 0 && y >= 0 && x < (N+2*M) && y < (N+2*M) && (map[x][y] != 1) && (map[x][y] != ELE);   
     }
     static void spread(int t){  
         // 기존 이상 현상은 사라지지 않고 지속됨
         // 시간 이상 현상 배열을 돌며 v의 배수일때 d 방향으로 한칸 씩 확장 -> 약간 장애물 처리 
         for(int i=0;i<F;++i){
             if(dead[i]) continue;
-            if(t == 0){ // 초기 설정
-                map[r[i]][c[i]] = ELE; // 이상현상
-            }else if(t % v[i] == 0){
-                if(!check2d(r[i] + dx[d[i]], c[i] + dy[d[i]])) {
+            if(t % v[i] == 0){
+                int nx = r[i] + dx[d[i]];
+                int ny = c[i] + dy[d[i]];
+                if(!check2d(nx,ny) || (map[nx][ny] == 4)) {
                     dead[i] = true; // 더이상 진행할 수 없는 이상현상
                     continue;
                 }
-                r[i] += dx[d[i]];
-                c[i] += dy[d[i]];
+                if(map[nx][ny] == PASS){
+                    while(map[nx][ny] == PASS){
+                        nx = nx + dx[d[i]];
+                        ny = ny + dy[d[i]];
+                    }
+                }
+                //System.out.printf("t : %d, (%d,%d) : %d -> (%d, %d) : %d \n", t, r[i], c[i], map[r[i]][c[i]], nx,ny, map[nx][ny]);
+                if(!check2d(nx,ny) || (map[nx][ny] == 4)) {
+                    dead[i] = true; // 더이상 진행할 수 없는 이상현상
+                    continue;
+                }
+                r[i] = nx;
+                c[i] = ny;
                 map[r[i]][c[i]] = ELE;
             }
         }
