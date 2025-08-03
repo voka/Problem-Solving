7700c8b9b93cd8232cd39198101d470a2f515561
voka <chdlswhd7@naver.com>
2025-08-03 23:37:31 +0900

    플로이드워셜문제

---
+++ OJS/2025/202508/P11265.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class P11265 {
+    static int N,M ;
+    static int[][] map;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        map = new int[N][N];
+        for(int i=0;i<N;++i){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0;j<N;++j){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+
+        }
+        for(int k=0;k<N;++k){
+            for(int i=0;i<N;++i){
+                for(int j=0;j<N;++j){
+                    if(map[i][j] > map[i][k] + map[k][j]){
+                        map[i][j] = map[i][k] + map[k][j];
+                    }
+                }
+            }
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for(int i=0;i<M;++i){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken()) - 1;
+            int b = Integer.parseInt(st.nextToken()) - 1;
+            int c = Integer.parseInt(st.nextToken());
+            if(map[a][b] <= c){
+                sb.append("Enjoy other party\n");
+            }else{
+                sb.append("Stay here\n");
+            }
+        }
+        System.out.println(sb.toString());
+
+    }
+}
